76a7f7f140ed97be54b28368bf00f1236aa56756
Philippe O. Wagner <wagner@arteria.ch>
2013-10-22 15:57:42 +0200

    Examples in README, event and publishtime in model, event 'landing'

--- history/views.py
+++ history/views.py
@@ -3,10 +3,17 @@ from django.shortcuts import render_to_response
 
 from .models import History
  
-@login_required
-def displayHistory(request, user=None):
+
+def displayHistory(request, anUser=None):
 	'''
     Returns the history for an user or the anonymous history.
     '''
-    hs, created = History.objects.get_or_create(owner=user)
+    hs, created = History.objects.get_or_create(owner=anUser)
 	return render_to_response('history/index.html', {'hs': hs, 'created': created})
+
+
+def displayHistoryEvent(request, anUser=None, eventID=None):
+    '''
+    Detail (landing page) for a particular event.
+    '''
+    return render_to_response('history/event.html', {})
