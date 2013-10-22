78fa0c73881634a923df0a00ff911e1392add492
Philippe O. Wagner <wagner@arteria.ch>
2013-10-22 17:15:52 +0200

    Added rendering ..

--- history/templatetags/history_tags.py
+++ history/templatetags/history_tags.py
@@ -1,11 +1,54 @@
 from django import template
+from django.template import RequestContext
+from django.template import Context, Template
+from django.utils.timesince import timesince
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.utils.encoding import smart_str, smart_unicode
+from django.template.defaultfilters import safe
+from django.template.loader import render_to_string
+
+from datetime import datetime, timedelta
+    
+from history.models import History, HistoryEvent
 
 register = template.Library()
 
-#@register.filter
-#def lower(value):
-#    '''
-#    Converts a string into all lowercase
-#    '''
-#    return value.lower()
+
+@register.filter  
+def renderHistoryEvent(request, historyEvent):
+    '''
+    '''
+    try:
+        pass
+    except Exception, ex:
+        try:
+            if settings.DEBUG:
+                return "ERROR "+ str(timeLineEvent.id) + " - " + str(ex) 
+            return safe("<!-- ERROR: " + str(ex)  + "-->")
+             
+        except:
+            pass                                                              
+    return safe("NULL<!-- NULL -->")
     
+    
+@register.filter
+def showHistory(who, amount=10, pageIndex=1):
+    if who == "anonymous":
+        hs, created = History.objects.get_or_create()
+    else:
+        # who is an username 
+        hs, created = History.objects.get_or_create(owner.username=who)
+    if created:            
+        hs.save()
+    isFirst = (pageIndex == 1) # could be used to show a header
+    historyEvents = hs.events.filter(publish_timestamp__lte=datetime.utcnow()).exclude(is_hidden=True).exclude(is_internal=True).order_by("-event_timestamp")
+    paginator = Paginator(historyEvents, amount)
+    thisPage = paginator.page(pageIndex)
+    hasMore = thisPage.has_next()
+    listOfHistoryEvents = thisPage.object_list
+    
+    return safe(render_to_string('history/history.html', {'listOfHistoryEvents': listOfHistoryEvents,
+                                                          'hasMore': hasMore,
+                                                          'isFirst': isFirst,
+                                                          'pageIndex': pageIndex,
+                                                         }, context_instance=RequestContext(request)))
