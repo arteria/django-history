4a4fe2320d2e39ada9c90145e245949c18c0937f
Walter Renner <walter.renner@me.com>
2014-04-11 10:32:49 +0200

    display related object in the admin site

--- history/admin.py
+++ history/admin.py
@@ -9,6 +9,19 @@ class HistoryEventAdmin(admin.ModelAdmin):
     search_fields = ['content_type', 'slug', 'desc']
     list_filter = ('content_type', 'is_internal', 'is_hidden')
 
+    def related_object_admin(self, obj):
+        """ Display link to related object's admin """
+        try:
+            if obj.content_type and obj.object_id:
+                admin_url = reverse('admin:%s_%s_change' % (obj.content_type.app_label, obj.content_type.model), args=(obj.object_id,))
+                return '%s: <a href="%s">%s</a>' % (obj.content_type.model.capitalize(), admin_url, obj.content_object.__unicode__())
+        except Exception, ex:
+            pass
+        return _('No relative object')
+    related_object_admin.allow_tags = True
+    related_object_admin.short_description = _('Related object')
+
+
 
 class HistoryAdmin(admin.ModelAdmin):
     list_display = ('owner', )
